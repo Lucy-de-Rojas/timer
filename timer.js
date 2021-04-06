eb9eaaebee176a133375a61c5a383240b0c98a9e
lucyderojas <50797298+lucyderojas@users.noreply.github.com>
2021-04-06 10:56:13 +0100

    commit

--- timer.js
+++ timer.js
@@ -8,6 +8,11 @@ function changeBackground() {
     document.body.style.backgroundColor = 'red';
     button.value = 'I\'m not red';
 
+    setTimeout(()=>{
+        document.body.style.backgroundColor = 'rgb(157, 240, 240)';
+        button.value = 'hello button';
+    },5000);
+
 }
 
 
